2688de390e69ebad221590033b96b5172faee618
Muhammad Moaz <m-moaz@hotmail.com>
2017-02-01 14:59:30 +0300

    Initialize game table view cell

---
+++ Bout Time/GameCell.swift
@@ -0,0 +1,24 @@
+//
+//  GameCell.swift
+//  Bout Time
+//
+//  Created by Muhammad Moaz on 2/1/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+
+class GameCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
