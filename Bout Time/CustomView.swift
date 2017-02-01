24f6624c212deca1082e4f0c45aba3dcf52d2d5c
Muhammad Moaz <m-moaz@hotmail.com>
2017-02-01 23:21:54 +0300

    Create custom uiview class

---
+++ Bout Time/CustomView.swift
@@ -0,0 +1,27 @@
+//
+//  CustomView.swift
+//  Bout Time
+//
+//  Created by Muhammad Moaz on 2/1/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = 4.0
+    }
+
+}
